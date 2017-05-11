d05031c5b88962e7a2640d29a06b87ab82c7767a
Andeson J <nynonet@gmail.com>
2017-05-10 23:52:24 -0300

    Enviando o pacote JSON "imprimindo" na Serial da conexão.

--- ArduinoJson/ArduinoJson.ino
+++ ArduinoJson/ArduinoJson.ino
@@ -28,28 +28,40 @@
   implemente conforme seu projeto. 
 */
 int ligado = 13; //porta só para fazer o led piscar 
-String comandoJson = ""; 
 int TempoParaEnvio = 3000; //a cada 3s segundos envia os dados
 
 float valorTemp = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+ 
   Serial.begin(9600); // para usar a leitura serial. 
-  pinMode(ligado, OUTPUT);
+  pinMode(ligado, OUTPUT); //para fazer um led piscar... 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ //liga ou desliga o led só de teste. 
  digitalWrite( ligado, !digitalRead(ligado) );
  
- valorTemp = random(1, 200);
- 
- String tempertura = "{\"titulo\":\"Temperatura\", \"valor\":\""+ String(valorTemp) + "\" }";
- String ledligado = "{\"titulo\":\"Led Ligado?\", \"valor\":\""+ String(digitalRead(ligado)) + "\" }";
- delay(TempoParaEnvio);
- Serial.println( tempertura ); 
- Serial.println( ledligado ); 
+ valorTemp = random(-20, 50); //para gerar um valor aleatório. 
 
+ //Gera os dados da temperatura pra enviar como JSON
+ String temperatura = "{\"titulo\":\"Temperatura\", \"valor\":\""+ String(valorTemp) + "º\" }";
+ 
+ //Gera os dados do led pra enviar como JSON  1 = ligado; 0 = desligado
+ String ledligado = "{\"titulo\":\"Led Ligado?\", \"valor\":\""+ String(!digitalRead(ligado)) + "\" }";
+
+ //Gera os dados do led pra enviar como JSON  1 = ligado; 0 = desligado
+ String textoLivre = "{\"titulo\":\"Texto Livre\", \"valor\":\"Aqui posso também escrever um valor e enviar.\" }";
+
+ 
+ //tempo para o proximo ciclo "envio".
+ delay(TempoParaEnvio);
+
+ //Montando o pacote no formato esperado, passando cada grupo de informação.
+ //aqui pode colocar quantos grupos deseja. lembrando de colocar a "," entre cada grupo. 
+ String pacoteJSON = "{\"Leitura\":["+ temperatura + ","+ ledligado +","+ textoLivre +"]}";
+
+ //envia o comando..
+ Serial.println( pacoteJSON );
 
 }
